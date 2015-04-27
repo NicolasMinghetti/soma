4a019b9aef590ef3439be748c275eeaaafae660d
Camille <camille.clausse@gmail.com>
2015-04-27 10:09:11 +0200

    version de camille

---
+++ soma/more.py
@@ -0,0 +1,12 @@
+def save_profile(backend, user, response, *args, **kwargs):
+    if backend.name == 'facebook':
+        user_profile = user.get_profile()
+        if user_profile is None:
+            user_profile = UserProfile(user_id=user.id)
+        user_profile.facebook_access_token = response.get('access_token')
+        user_profile.locale = response.get('locale')
+        user_profile.gender = response.get('gender')
+        user_profile.email = response.get('email')
+        user_profile.facebook_link = response.get('facebook_link')
+        user_profile.facebook_id = response.get('facebook_id')
+        user_profile.save() 
---
+++ soma/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+
+urlpatterns = patterns(
+    '',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('app.urls')),
+    url(r'', include('social.apps.django_app.urls', namespace='social')),
+
+)
