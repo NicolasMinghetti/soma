b7e3b0d9bb47ae948bf4af21f6fbd5eac3a3bdeb
nico691 <nicolas.minghetti@gmail.com>
2015-05-13 11:44:19 +0200

    Formulaire objectif

--- app/models.py
+++ app/models.py
@@ -5,8 +5,18 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True, related_name='profile')
     photo = models.TextField()
-
-
+    objectif1 = models.CharField(max_length=10)
+    objectif1a=models.CharField(max_length=30)
+    objectif1b=models.CharField(max_length=30)
+    objectif1c=models.CharField(max_length=30)
+    objectif2 = models.CharField(max_length=10)
+    objectif2a=models.CharField(max_length=30)
+    objectif2b=models.CharField(max_length=30)
+    objectif2c=models.CharField(max_length=30)
+    objectif3 = models.CharField(max_length=10)
+    objectif3a=models.CharField(max_length=30)
+    objectif3b=models.CharField(max_length=30)
+    objectif3c=models.CharField(max_length=30)
 #old tutorial
 # class Post(models.Model):
 #     author = models.ForeignKey('auth.User')
--- app/views.py
+++ app/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.contrib.auth import logout
 from django.shortcuts import redirect
+from .forms import NameForm
+from .models import UserProfile
 
+from django.http import HttpResponseRedirect
 
 def index(request):
     if request.user :
@@ -21,19 +24,107 @@ def index(request):
 def graphe(request):
     return render(request, 'graphe.html', {})
 
-def objectif(request):
-    current_path=get_current_path(request)
-    if request.user.is_authenticated():
-        print("bite")
-    return render(request, 'objectif.html', {'current_path':current_path})
-
+#def objectif(request):
+#    current_path=get_current_path(request)
+#    if request.user.is_authenticated():
+#        print("bite")
+#    return render(request, 'objectif.html', {'current_path':current_path})
+#
 def objectifChoisir(request):
     current_path=get_current_path(request)
     return render(request, 'objectifChoisir.html', {'current_path':current_path})
 
+def name(request):
+    current_path=get_current_path(request)
+    return render(request, 'name.html', {'current_path':current_path})
+
+def objectif(request):
+    current_path=get_current_path(request)
+    profile=UserProfile.objects.get(user=request.user)
+
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            print(request.POST['choix'])
+            if profile.objectif1=="Null":
+                profile.objectif1=request.POST['choix']
+                profile.objectif1a=request.POST['choix1']
+                profile.objectif1b=request.POST['choix2']
+                profile.objectif1c=request.POST['choix3']
+            elif profile.objectif2=="Null":
+                profile.objectif2=request.POST['choix']
+                profile.objectif2a=request.POST['choix1']
+                profile.objectif2b=request.POST['choix2']
+                profile.objectif2c=request.POST['choix3']
+            else:
+                profile.objectif3=request.POST['choix']
+                profile.objectif3a=request.POST['choix1']
+                profile.objectif3b=request.POST['choix2']
+                profile.objectif3c=request.POST['choix3']
+            profile.save()
+            print("On sauvegarde...")
+            # redirect to a new URL:
+            print("Debut ojectif")
+            print(profile.objectif1)
+            print(profile.objectif2)
+            print(profile.objectif3)
+            print("/fin objectif")
+
+
+            return HttpResponseRedirect('/VosObjectifs/')
+        else:
+            return render(request, 'erreur.html', {'message':'Tu n\'as pas rempli le formulair corectement !'})
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        return render(request, 'objectif.html', {'current_path':current_path})
+
 def recherche(request):
     current_path=get_current_path(request)
     return render(request, 'recherche.html', {'current_path':current_path})
+
+def vosObjectifs(request):
+    current_path=get_current_path(request)
+    profile=UserProfile.objects.get(user=request.user)
+    return render(request, 'VosObjectifs.html', {'current_path':current_path,'objectif1':profile.objectif1,'objectif1a':profile.objectif1a,'objectif1b':profile.objectif1b,'objectif1c':profile.objectif1c,'objectif2':profile.objectif2,'objectif2a':profile.objectif2a,'objectif2b':profile.objectif2b,'objectif2c':profile.objectif2c,'objectif3':profile.objectif3,'objectif3a':profile.objectif3a, 'objectif3b':profile.objectif3b, 'objectif3c':profile.objectif3c})
+
+def vosObjectifsSupprimer1(request):
+    current_path=get_current_path(request)
+    profile=UserProfile.objects.get(user=request.user)
+
+    profile.objectif1="Null"
+    profile.objectif1a="Vous n'avez pas défini d'objectif"
+    profile.objectif1b=""
+    profile.objectif1c=""
+    profile.save()
+    return HttpResponseRedirect('/VosObjectifs/')
+
+def vosObjectifsSupprimer2(request):
+    current_path=get_current_path(request)
+    profile=UserProfile.objects.get(user=request.user)
+
+    profile.objectif2="Null"
+    profile.objectif2a="Vous n'avez pas défini d'objectif"
+    profile.objectif2b=""
+    profile.objectif2c=""
+    profile.save()
+    return HttpResponseRedirect('/VosObjectifs/')
+
+def vosObjectifsSupprimer3(request):
+    current_path=get_current_path(request)
+    profile=UserProfile.objects.get(user=request.user)
+
+    profile.objectif3="Null"
+    profile.objectif3a="Vous n'avez pas défini d'objectif"
+    profile.objectif3b=""
+    profile.objectif3c=""
+    profile.save()
+    return HttpResponseRedirect('/VosObjectifs/')
 
 def coach(request):
     current_path=get_current_path(request)
